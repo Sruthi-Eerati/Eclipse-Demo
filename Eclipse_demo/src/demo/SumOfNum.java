aebfee54b0daaaaa9a55b937155adccb2c8da188
Sruthi-Eerati <sruthiyadav2002@gmail.com>
2024-10-16 19:04:54 +0530

    added new file

---
+++ Eclipse_demo/src/demo/SumOfNum.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class SumOfNum {
+
+	public static void main(String[] args) {
+		int a =8;
+		int b =3;
+		int sum= a+b;
+		System.out.println("sum of two numbers : " + sum);
+	}
+
+}
